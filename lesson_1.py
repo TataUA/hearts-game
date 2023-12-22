26f92f53e6eac6863e90a0c50729bb1a5d66ccd7
TataUA <khyzhnjaktetjana@gmail.com>
2023-12-22 11:34:56 +0200

    added main project files

--- lesson_1.py
+++ lesson_1.py
@@ -1,4 +1,4 @@
-#️Домашнє завдання:
+# ️Домашнє завдання:
 # написати функцію якам приймає рядок і повертає словник у якому
 # ключами є всі символи, які зустрічаються в цьому рядку, а значення - відповідні вірогідності зустріти цей символ в цьому рядку.
 # код повинен бути структурований за допомогою конструкції if name == "main":,
@@ -8,7 +8,8 @@
 from typing import Dict
 from collections import Counter
 
-def probability_simbols (text: str) -> Dict:
+
+def probability_simbols(text: str) -> Dict:
     """
     Функція 'probability_simbols' розраховує вірогідність зустрічі кожного символу в рядку.
     Параметри:
@@ -17,17 +18,19 @@ def probability_simbols (text: str) -> Dict:
         dict: словник, ключами якого є символи, а значеннями-вірогідність зустріти ці символи.
     """
 
-    #Підрахуємо частоту зустрічі однакових символів у рядку за доп. класу 'Counter' модуля 'collections'
+    # Підрахуємо частоту зустрічі однакових символів у рядку за доп. класу 'Counter' модуля 'collections'
     simbols_counter = Counter(text)
 
-    #Знайдемо довжину рядку
+    # Знайдемо довжину рядку
     text_length: int = len(text)
 
-    #Визначаємо вірогідність зустрічі символу та записуємо до словника
-    simbols_dict: Dict[str, float] = {simbol_key: round(text.count(simbol_key) / text_length * 100, 2) for simbol_key in set(text)}
-#або
-    simbols_dict2: Dict[str, float] = {simbol_key: round(count / text_length * 100, 2) for simbol_key, count in simbols_counter.items()}
-    return (simbols_dict, simbols_dict2)
+    # Визначаємо вірогідність зустрічі символу та записуємо до словника
+    simbols_dict: Dict[str, float] = {
+        simbol_key: round(count / text_length * 100, 2)
+        for simbol_key, count in simbols_counter.items()
+    }
+    return simbols_dict
+
 
 if __name__ == "__main__":
     string = "Hello World!"
@@ -44,6 +47,8 @@ if __name__ == "__main__":
 x = 0
 y = 0
 
-result = "game over" if x == 0 and y == 0 else (0 if x == y else (x + y if x < y else x - y))
+result = (
+    "game over" if x == 0 and y == 0 else (0 if x == y else (x + y if x < y else x - y))
+)
 
 print(result)
